fd03d9884307400f9f7c7ac428249b31e0b1dc45
Sam O'Donnell <samuel.odonnell1@gmail.com>
2020-07-18 09:58:11 +1000

    Fixed port variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // express app
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // API setup
 const lib = require('lib')({token: 'tok_prod_yJp1jRKWRXRUSmCYNG142R28VCaMQT5kpZ3eHQiC84rxR6mhskkHJMrs9Y4FLtx9'});
